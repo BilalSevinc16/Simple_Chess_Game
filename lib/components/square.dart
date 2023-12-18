772cfb597ab0631af245af6a5707d274074b0a69
Bilal Sevinç <BilalSevinctr@hotmail.com>
2023-12-19 01:57:40 +0300

    Simple_Chess_Game
    Simple Chess Game

---
+++ lib/components/square.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:simple_chess/components/piece.dart';
+import 'package:simple_chess/values/colors.dart';
+
+class Square extends StatelessWidget {
+  final bool isWhite;
+  final ChessPiece? piece;
+  final bool isSelected;
+  final bool isValidMove;
+  final void Function()? onTap;
+
+  const Square({
+    Key? key,
+    required this.isWhite,
+    required this.piece,
+    required this.isSelected,
+    required this.onTap,
+    required this.isValidMove,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color? squareColor;
+    // if selected, square is green
+    if (isSelected) {
+      squareColor = Colors.green;
+    } else if (isValidMove) {
+      squareColor = Colors.green.shade300;
+    }
+
+    // otherwise, it's white or black
+    else {
+      squareColor = isWhite ? foregroundColor : backgroundColor;
+    }
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: squareColor,
+        margin: EdgeInsets.all(isValidMove ? 8 : 0),
+        child: piece != null
+            ? Image.asset(
+                piece!.imagePath,
+                color: piece!.isWhite ? Colors.white : Colors.black,
+              )
+            : null,
+      ),
+    );
+  }
+}
